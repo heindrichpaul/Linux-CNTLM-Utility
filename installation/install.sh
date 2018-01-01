655e4d5379f8c4cb952fb6f242056855630b8f43
Heindrich Paul <heindrich.paul@gmail.com>
2018-01-01 19:36:47 +0200

    Added installation script

---
+++ installation/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Written by Heindrich Friedrich Paul
+# Except explicitly stated, all right reserved by Heindrich Friedrich Paul and 
+# the script is licensed under the MIT license.
+
+sudo mv changecntlmpassword /bin/
+sudo chmod 755 /bin/changecntlmpassword
+sudo chown root:root /bin/changecntlmpassword
